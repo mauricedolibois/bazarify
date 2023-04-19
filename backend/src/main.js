54d0041c8aaaadbfa3080c7dc79d278123751249
mb389 <mb389@hdm-stuttgart.de>
2023-04-19 20:16:02 +0200

    Added Test for Frontend Connection Add and get object from db

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,13 +1,25 @@
 import express from 'express'
 const app = express()
-import { dbConnection } from './dbConnection.js'
+import { dbConnection } from './DB/dbConnection.js'
 
 
-app.get("/api", (req, res) => {
-    res.json({"backendData": [  "Das", "Kommt", "ausm", "Backend!"] })
-})
+
+
 
 dbConnection.connectToDB().then(r => { console.log("Fertig!") })
 
-//console.log(db)
+const doc = {"name": "Nordica Dobermann GSR", "price": 899 }
+const result = dbConnection.insertOne("articles", doc)
+console.log(`A document was inserted with the _id: ${result.insertedId}`);
+
+
+
+app.get("/api", (req, res) => {
+    // res.json({"backendData": [  "Das", "Kommt", "ausm", "Backend!"] })
+    const getDoc = dbConnection.findOne("articles", {"_id": 1}).then(r => {
+        res.json({"backendData": [  r.name, r.price] })
+    })
+})
 app.listen(8085, () => { console.log("Server started on port 8085") })
+
+
