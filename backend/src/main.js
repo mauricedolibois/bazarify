82e521f2303a8efec164d548b61fe1e2ee705049
jb266 <jb266@hdm-stuttgart.de>
2023-04-26 01:34:28 +0200

    Add Component btnSubmit

--- backend/src/main.js
+++ backend/src/main.js
@@ -4,13 +4,21 @@ import { ProductManager } from './manager/ProductManager.js'
 
 ProductManager.connectToDB()
 //Insert into DB
-ProductManager.addProduct(5,"Samu", 420)
+//ProductManager.addProduct(5,"Samu", 420)
 
 //Get from DB
 app.get("/api", (req, res) => {
     ProductManager.getProductById(5).then(product => {
     res.json({"backendData": [product.name, product.price] })
 })
+})
+
+// Add new product to DB
+app.post("/api/add-product", (req, res) => {
+    const { id, name, price } = req.body;
+    ProductManager.addProduct(id, name, price)
+      .then(() => res.status(200).json({ message: 'Product added successfully' }))
+      .catch(error => res.status(500).json({ message: 'Failed to add product', error }));
 })
 
 app.listen(8085, () => { console.log("Server started on port 8085") })
---
+++ frontend/bazarify/components/inputField.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TextInput = ({ label, name, value, onChange }) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <input 
+        type="text" 
+        name={name}
+        value={value} 
+        onChange={onChange} />
+    </div>
+  );
+};
+
+export default TextInput;
