6de2b63a2d037544a40d26a7919d745dbcdb68a8
jb266 <jb266@hdm-stuttgart.de>
2023-04-26 01:25:21 +0200

    Add TextField and BUtton Component

--- frontend/bazarify/pages/_app.js
+++ frontend/bazarify/pages/_app.js
@@ -1,23 +1,62 @@
-import '@/styles/globals.css'
-import { useState, useEffect } from 'react'
+import React, { useState } from 'react'
+import TextInput from '../components/inputField'
+import SubmitButton from '../components/btnSubmit'
 
 export default function App({ Component, pageProps }) {
-  const [backendData, setBackendData] = useState([{}])
+  const [inputValues, setInputValues] = useState({ id: '', name: '', price: '' });
+  const [backendData, setBackendData] = useState(undefined);
 
-  useEffect(() => {
-    fetch("/api").then(
-      response =>  response.json()
-    ).then(
-      data => { setBackendData(data) }
-    )
-  }, [])
+  const handleTextChange = (event) => {
+    const { name, value } = event.target;
+    console.log(name, value)
+    setInputValues((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  
+  
 
-  return <>
-    <Component {...pageProps} />
-    <h1>Hallo Maurice!</h1>
-    <div>
-      {(typeof backendData.backendData === 'undefined') ? (<p>loading...</p>) : (backendData.backendData.map((data, i) => <p key={i}>{data}</p>))}
-    </div>
+  /*const handleFormSubmit = (event) => {
+    console.log("LOG: handeFormSubmit triggert")
+    event.preventDefault();
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(inputValues)
+    };
+    fetch('/api/add-product', requestOptions)
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+      console.log("LOG: Form sent!")
+    };
+  */
 
-  </>
+    const handleFormSubmit = (event) => {
+      event.preventDefault();
+      console.log(inputValues);
+    };
+
+    //TODO: fix submit 
+
+
+  return (
+    <>
+      <Component {...pageProps} />
+      <h1>Hallo Maurice!</h1>
+      <div>
+        {(backendData && typeof backendData.backendData === 'undefined') ? (<p>loading...</p>) : (backendData?.backendData?.map((data, i) => <p key={i}>{data}</p>))}
+      </div>
+
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <TextInput label="ID des Artikels:" name="id" value={inputValues.id} onChange={handleTextChange} />
+          <TextInput label="Name des Artikels:" name="name" value={inputValues.name} onChange={handleTextChange} />
+          <TextInput label="Preis des Artikels:" name="price" value={inputValues.price} onChange={handleTextChange} />
+        </div>
+        <SubmitButton />
+      </form>
+    </>
+  )
 }
