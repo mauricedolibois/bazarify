43e989f46eebf4c9824e513498e0e50c7714c424
Maurice Dolibois <md147@hdm-stuttgart.de>
2023-05-13 21:42:29 +0200

    added remaining dbfunctions, update in progress

--- backend/src/controller/products.js
+++ backend/src/controller/products.js
@@ -19,8 +19,6 @@ productRouter.use(cors({
 //      })
 //      .catch(error => console.log(error))
 //  }, [id])//id hier rein (dependency array) damit useEffect bei jeder änderung von id triggert
-
- 
 productRouter.get("/product", (req, res) => {
     dbConnection.findProduct(req.query.operator, req.query.parameter).then(product => {
         res.send(product)
@@ -39,7 +37,6 @@ productRouter.get("/product", (req, res) => {
 //       })
 //       .catch(error => console.log(error))
 //   }, [])
- 
 productRouter.get("/allProducts", (req, res) => {
     dbConnection.findAllProducts().then(product => {
         res.send(product)
@@ -47,25 +44,50 @@ productRouter.get("/allProducts", (req, res) => {
 }
 )
 
-  // //PostProduct
-  // useEffect(() => {
-  //   const requestOptions = {
-  //     method: 'POST',
-  //     headers: { 'Content-Type': 'application/json' },
-  //     body: JSON.stringify({ name: 'Ski', price: 100 })
-  // };
-  //   fetch('http://localhost:8085/api/product', requestOptions)
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       console.log(data)
-  //     })
-  //     .catch(error => console.log(error))
-  // }, [])
- 
+ // //PostProduct
+//  const name = "Ski"
+//  const price = 100
+//  const category = "Sport"
+//  useEffect(() => {
+//    const requestOptions = {
+//      method: 'POST',
+//      headers: { 'Content-Type': 'application/json' },
+//      body: JSON.stringify({ name: name, price: price, category: category })
+//  };
+//    fetch('http://localhost:8085/api/product', requestOptions)
+//      .then(res => res.json())
+//      .then(data => {
+//        console.log(data)
+//      })
+//      .catch(error => console.log(error))
+//  }, [])
+productRouter.post("/product", (req, res) => {
+    dbConnection.insertProduct(req.body.name, req.body.price, req.body.category).then
+    (product => { res.send(product) })  
+    })
 
-// productRouter.post("/product", (req, res) => {
-//     dbConnection.insertProduct(req.query.name, req.query.price).then
-//     (product => { res.send(product) })  
+
+//deleteProduct
+// const operator = "product_id"
+// const id=25250
+// useEffect(() => {
+//   fetch('http://localhost:8085/api/product?operator='+[operator]+'&parameter='+id, {method: 'DELETE'})
+//     .then(res => res.json())
+//     .then(data => {
+//       console.log(data)
 //     })
+//     .catch(error => console.log(error))
+// }, [])    
+productRouter.delete("/product", (req, res) => {
+    dbConnection.deleteProduct(req.query.operator, req.query.parameter).then
+    (product => { res.send(product) })  
+    })
+
+
+productRouter.put("/product", (req, res) => {
+    dbConnection.updateProduct(req.query.operator, req.query.parameter, req.body).then
+    (product => { res.send(product) })
+    })
+
 
 
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,10 +3,41 @@ import TextInput from '../components/formInput'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
+  const [product, setProduct] = useState('')
+
+  useEffect(() => {
+        fetch('http://localhost:8085/api/allProducts', {method: 'GET'})
+          .then(res => res.json())
+          .then(data => {
+            console.log(data)
+            setProduct(JSON.stringify(data))
+          })
+          .catch(error => console.log(error))
+      }, [])
+
+  //updateProduct
+  const newProduct={name: "SKYYYY", price: 1, category: "sort"}
+  const operator = "product_id"
+  const id=950966
+  useEffect(() => {
+    const requestOptions = {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newProduct)
+  };
+    fetch('http://localhost:8085/api/product?operator='+[operator]+'&parameter='+id, requestOptions)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
+      .catch(error => console.log(error))
+  }, [])
+  
 
   return (
     <>
       <Component {...pageProps} />
+      <p>{product}</p>
     </>
   )
 }
