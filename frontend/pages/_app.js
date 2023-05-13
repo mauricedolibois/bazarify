243bda674fc6204abc41b51ea784412480c00f12
mb389 <mb389@hdm-stuttgart.de>
2023-05-13 18:51:49 +0200

    deleted statments of old code

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,14 +3,7 @@ import TextInput from '../components/formInput'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  const [product, setProduct] = useState("");
-  const [allProducts, setAllProducts] = useState("");
-  const [id, setID] = useState("");
-  
 
-    
-
-   
   return (
     <>
       <Component {...pageProps} />
