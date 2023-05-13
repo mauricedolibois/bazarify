db2c80d89adb02aed39bdde9b3c82507f85b95ee
mb389 <mb389@hdm-stuttgart.de>
2023-05-13 12:13:51 +0200

    Added DbIdHandler, for create unique Ids

--- backend/src/database/DbConnection.js
+++ backend/src/database/DbConnection.js
@@ -1,11 +1,11 @@
-import { MongoClient } from 'mongodb'
 import { createRequire } from "module"
 const require = createRequire(import.meta.url)
-import express from 'express'
 import mongoose from 'mongoose'
 const Product = require('./schemas/ProductSchema.cjs')
 const Customer = require('./schemas/CustomerSchema.cjs')
 const Sale = require('./schemas/SaleSchema.cjs')
+const DbIdHandler = require('./DbIdHandler.cjs')
+
 
 
   export const dbConnection = {
@@ -19,8 +19,12 @@ const Sale = require('./schemas/SaleSchema.cjs')
     async close() { 
         await mongoose.connection.close().then(console.log("DB closed"))
     },
-    async insertProduct(id, name, price) {
+    async insertProduct(name, price) {
         try{
+            var id = DbIdHandler.generateProductId()
+            while((await this.findProduct('product_id', id)).length != 0){
+                id = DbIdHandler.generateProductId()
+            }
             const product = await Product.create({product_id: id, product_name : name, product_price: price})
             await product.save().then(console.log(product))
         }
@@ -28,15 +32,23 @@ const Sale = require('./schemas/SaleSchema.cjs')
     },
     async insertCustomer(name, firstname, email, phone) {
         try{
-            const customer = await Customer.create({customer_name : name, customer_firstname: firstname, customer_email: email, customer_phone: phone})
+            var id = DbIdHandler.generateCustomerId()
+            while((await this.findProduct('customer_id', id)).length != 0){
+                id = DbIdHandler.generateCustomerId()
+            }
+            const customer = await Customer.create({customer_id: id,customer_name : name, customer_firstname: firstname, customer_email: email, customer_phone: phone})
             await customer.save().then(console.log(customer))
         }
         catch{console.log("customer already exists")}
     },
-    async insertSale(id, product_id, customer_id) {
+    async insertSale(product_id, customer_id) {
         try{
-        const sale = await Sale.create({order_id: id, product_id : product_id, customer_id: customer_id})
-        await sale.save().then(console.log(sale))
+            var id = DbIdHandler.generateProductId()
+            while((await this.findProduct('product_id', id)).length != 0){
+                id = DbIdHandler.generateProductId()
+            }
+            const sale = await Sale.create({order_id: id, product_id : product_id, customer_id: customer_id})
+            await sale.save().then(console.log(sale))
         }
         catch{console.log("sale already exists")}
     },
--- backend/src/main.js
+++ backend/src/main.js
@@ -19,11 +19,11 @@ dbConnection.connectToDB()
 //dbConnection.deleteAllSales()
 //dbConnection.close()
 
-dbConnection.insertProduct(1,"test", 69)
+dbConnection.insertProduct("test", 69)
 dbConnection.insertCustomer("bucher", "maik", "maik@bucher.de", 12345)
 dbConnection.insertSale(1,1,1)
 
-dbConnection.findProduct("product_id", 1).then(product => {console.log(product)})
+//dbConnection.findProduct("product_id", 1).then(product => {console.log(product)})
 //dbConnection.updateProduct("product_id", 1, {product_name: "test2"})
 //dbConnection.findAllCustomers().then(customer => {console.log(customer)})
 
