460b87f3562a0c7c4968c2955c6383050b7d1bfd
jb266 <jb266@hdm-stuttgart.de>
2023-05-14 18:17:10 +0200

    change properties for underlinendInput component

--- frontend/components/underlinedInput.js
+++ frontend/components/underlinedInput.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const UnderlindedInput = ({placeholder}) => {
+const UnderlindedInput = ({id, placeholder, value, onChange}) => {
   return (
     <div class="group flex items-center">
         <input class="w-full truncate border-b border-ourLightGray text-ourDarkGray focus:border-ourPrimaryColor focus:outline-none" 
-            type="text" placeholder={placeholder} />
+            name={id} id={id} type="text" value={value} placeholder={placeholder} onChange={onChange}/>
     </div>
   );
 };
