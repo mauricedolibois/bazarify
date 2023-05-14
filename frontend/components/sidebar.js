cbd9027cccff3fcdcef1b977a6ce795d7c807b09
Samuel Riester <riestersamuel@icloud.com>
2023-05-14 15:16:19 +0200

    Worked on contexts in the frontend

--- frontend/components/sidebar.js
+++ frontend/components/sidebar.js
@@ -2,16 +2,51 @@ import { UilAngleRight } from '@iconscout/react-unicons'
 import { UilApps } from '@iconscout/react-unicons'
 import { UilPlus } from '@iconscout/react-unicons'
 import { UilQuestionCircle } from '@iconscout/react-unicons'
+import Link from 'next/link'
+import React from 'react'
+import { useContext } from 'react'
+import { BazarContext } from '../pages/index.js'
 
-export default function () {
+function Step({ step, text, currentStep }) {
+    let stepClass = "text-sm text-ourGray";
+    if (step === currentStep) {
+        stepClass = "text-sm text-ourPrimaryColor font-bold";
+    } else if (step < currentStep) {
+        stepClass = "text-sm line-through";
+    }
+
+    return (
+        <li className="flex items-center">
+            <span className={stepClass}>
+                {step}. {text}
+            </span>
+        </li>
+    );
+}
+
+function goToNextStep({ step, currentStep }) {
+    // If step < 5 then go to next step and show the text "Nächster Schritt"
+    // If step == 5 then go to step 1 and show the text "Basar abschließen"
+    return ({
+
+    })
+}
+
+export default function Sidebar() {
+
+    const { step, setStep, newBazar, setNewBazar, createBazar } = useContext(BazarContext)
+
+
     return (
         <>
             <div class="flex h-screen flex-col bg-white min-w-[25%] max-w-md">
 
                 <div class="flex items-center justify-between pt-4">
                     <div class="flex items-center cursor-pointer">
-                        <span class="pl-4 font-serif text-2xl text-ourPrimaryColor">B</span>
-                        <span class="font-serif text-2xl">azarify</span>
+                        <Link href="/">
+                            <span class="pl-4 font-serif text-2xl text-ourPrimaryColor">B</span>
+                            <span class="font-serif text-2xl">azarify</span>
+                        </Link>
                     </div>
                     <button class="flex items-center pr-4 text-sm text-black">
                         <UilQuestionCircle size="24" />
@@ -20,15 +55,17 @@ export default function () {
 
 
                 <div class="flex flex-row items-center justify-center px-4 py-4">
-                    <button class="border-ourGrey text-ourGrey flex flex-col justify-center rounded-lg border-2 px-4 py-2 flex-1">
-                        <UilPlus class="" size="24" />
-                        <span class="text-ourGrey text-sm mt-4">Neuer Bazar</span>
-                    </button>
+                    <Link href="/template" className='flex-1'>
+                        <button class="border-ourGrey text-ourGrey rounded-lg border-2 flex flex-col justify-center px-4 py-2 flex-1">
+                            <UilPlus class="" size="24" />
+                            <span class="text-ourGrey text-sm mt-4">Neuer Bazar</span>
+                        </button>
+                    </Link>
                     <button class="border-ourGrey text-ourGrey ml-2 flex flex-col justify-center rounded-lg border-2 px-4 py-2 flex-1">
                         <UilApps class="" size="24" />
                         <span class="text-ourGrey text-sm mt-4">Bazare verwalten</span>
                     </button>
-                </div>
+                </div >
 
 
 
@@ -37,26 +74,11 @@ export default function () {
                 <h3 class="px-4 py-2 text-lg font-bold text-ourSuperDarkGray">Name des Basars</h3>
 
                 <ul class="flex flex-grow flex-col space-y-2 px-4 py-2">
-                    <li class="flex items-center line-through cursor-pointer">
-                        <span class="text-sm">1. Basar erstellen</span>
-                    </li>
-                    <li class="flex items-center">
-                        <div class="relative flex items-center">
-                            <div class="left-0 -ml-6 h-4 w-4 rounded-full bg-ourPrimaryColor">
-                                <div class="h-full w-full -translate-x-1/2 transform bg-white"></div>
-                            </div>
-                            <p class="text-sm font-bold ml-2 text-ourPrimaryColor">2. Annahme</p>
-                        </div>
-                    </li>
-                    <li class="flex items-center">
-                        <span class="text-sm text-ourGray">3. Verkauf</span>
-                    </li>
-                    <li class="flex items-center">
-                        <span class="text-sm text-ourGrey">4. Abholung</span>
-                    </li>
-                    <li class="flex items-center">
-                        <span class="text-sm text-ourGrey">5. Analytics und Bilanz</span>
-                    </li>
+                    <Step step="1" text="Neuer Basar" currentStep="2" />
+                    <Step step="2" text="Annahme" currentStep="2" />
+                    <Step step="3" text="Verkauf" currentStep="2" />
+                    <Step step="4" text="Abholung" currentStep="2" />
+                    <Step step="5" text="Analytics und Bilanz" currentStep="2" />
                 </ul>
 
 
@@ -66,7 +88,7 @@ export default function () {
                         Nächster Schritt
                     </button>
                 </div>
-            </div>
+            </div >
         </>
     )
 }
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -6,6 +6,10 @@ export default function App({ Component, pageProps }) {
       // Hier wird am Anfang der Index reingerendet. Hier können wir Komponenten darstellen, die auf jeder Seite gleich sind, typischerweise Dinge wie ein Footer.
       // The Component prop is the active page, so whenever you navigate between routes, Component will change to the new page. Therefore, any props you send to Component will be received by the page. Source: https://nextjs.org/docs/pages/building-your-application/routing/custom-app
     }
-    <Component {...pageProps} />
+    <div className="flex flex-row">
+      <div className="">
+        <Component {...pageProps} />
+      </div>
+    </div>
   </>
 }
