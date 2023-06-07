b828e4ef45a834e35e17aefa96ce1657406b845c
Samuel Riester <riestersamuel@icloud.com>
2023-06-07 10:38:57 +0200

    Added a help popup and a lots of UI improvements

---
+++ frontend/components/HelpPopup.js
@@ -0,0 +1,48 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { UilMultiply, UilQuestionCircle } from '@iconscout/react-unicons'
+
+const PopupTemplate = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const popupRef = useRef(null);
+
+    const openPopup = () => {
+        setIsOpen(true);
+    };
+
+    const closePopup = () => {
+        setIsOpen(false);
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (popupRef.current && !popupRef.current.contains(event.target)) {
+                closePopup();
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    return (
+        <div>
+            <button class="flex items-center pr-4 text-sm text-black hover:text-ourPrimaryColor" onClick={openPopup}>
+                <UilQuestionCircle size="24" />
+            </button>
+            {isOpen && (
+                <div className='fixed w-screen h-screen top-0 left-0 bg-black/75 flex justify-center items-center'>
+                    <div className='w-1/2 h-1/2 bg-white rounded-lg relative p-8' ref={popupRef}>
+                        <UilMultiply className='absolute top-4 right-4 cursor-pointer' onClick={closePopup} />
+                        <h1>Hilfe</h1>
+                        <p>Wenn Sie Hilfe benötigen melden Sie sich bitte an sr185@hdm-stuttgart.de.</p>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default PopupTemplate;
