46ce478161d631f3173f760fa6e8bec498d16d1e
Samuel Riester <riestersamuel@icloud.com>
2023-06-07 09:28:59 +0200

    Implemented first prototype for pie chart

--- frontend/components/SellerInformation.js
+++ frontend/components/SellerInformation.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import UnderlindedInput from './NewProductInput';
+import ButtonSmallJustIcon from './buttons/ButtonSmallJustIcon';
+import { UilSearch } from '@iconscout/react-unicons'
+import FormInput from './formInput';
 
 function SellerInformation() {
     // Annahme: Der Name, der Erlös und die Anzahl der nicht verkauften Produkte werden aus dem Backend geholt
@@ -16,23 +20,30 @@ function SellerInformation() {
     };
 
     return (
-        <div class="grid grid-cols-3 bg-white rounded border-ourLightGray border ">
-            <div class="flex justify-center items-center py-4">
-                <p>{name}</p>
+        <>
+            <div className='flex gap-4'>
+                <div class="">
+                    <div class="relative mt-2 rounded-md shadow-sm">
+                        <div class="pointer-events-none absolute inset-y-0 left-2 flex items-center"></div>
+                        <input type="text" class="block w-full rounded-md border-0 py-1.5 pl-4 pr-20 text-ourSuperDarkGray ring-1 ring-inset ring-ourLightGray placeholder:text-ourGray focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6" placeholder="Verkäufer suchen" />
+                    </div>
+                </div>
+                <ButtonSmallJustIcon icon={<UilSearch></UilSearch>}></ButtonSmallJustIcon>
             </div>
-            <div class="flex justify-center flex-col items-center border-l border-ourLightGray border-r py-4">
-                <p className='font-semibold'>{revenue}€</p>
-                <p className='mt-4'>Erlös</p>
+
+            <div class="grid grid-cols-3 mt-4 bg-white rounded border-ourLightGray border ">
+                <div class="flex justify-center items-center py-4">
+                    <p>{name}</p>
+                </div>
+                <div class="flex justify-center flex-col items-center border-l border-ourLightGray border-r py-4">
+                    <p className='font-semibold'>{revenue}€</p>
+                    <p className='mt-4'>Erlös</p>
+                </div>
+                <div class="flex justify-between text-center items-center py-4 px-8">
+                    <p>{getUnsoldProductsText()}</p>
+                </div>
             </div>
-            <div class="flex justify-between text-center items-center py-4 px-8">
-                <p>{getUnsoldProductsText()}</p>
-            </div>
-        </div>
-
-
-
-
-
+        </>
     );
 };
 
