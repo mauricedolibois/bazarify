6959a2fda72438dd9d88691c7637b5a3a2d2e539
Samuel Riester <riestersamuel@icloud.com>
2023-06-17 20:44:27 +0200

    Bug fix for step3 table (category is now also shown)

--- frontend/components/step3TableRow.js
+++ frontend/components/step3TableRow.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { UilTrash } from '@iconscout/react-unicons'
 import ButtonSmallJustIcon from './buttons/ButtonSmallJustIcon';
 
-const step3TableRow = ({ counter, name, categorie, price, removeItem }) => {
+const step3TableRow = ({ counter, name, category, price, removeItem }) => {
   return (
     <tr>
       <td class="whitespace-nowrap px-8 py-4">{counter}</td>
       <td class="whitespace-nowrap px-8 py-4">{name}</td>
-      <td class="whitespace-nowrap px-8 py-4">{categorie}</td>
+      <td class="whitespace-nowrap px-8 py-4">{category}</td>
       <td class="whitespace-nowrap px-8 py-4">{price}€</td>
       <td class="whitespace-nowrap px-8 py-4"><button className='hover:text-ourPrimaryColorHover' onClick={removeItem}><UilTrash size="17"></UilTrash></button></td>
     </tr>
