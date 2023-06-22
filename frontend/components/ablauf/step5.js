be88fb4413e5230402cc556d39edf5755f2d998a
jb266 <jb266@hdm-stuttgart.de>
2023-06-22 15:05:14 +0200

    display analytics from backend

--- frontend/components/ablauf/step5.js
+++ frontend/components/ablauf/step5.js
@@ -10,20 +10,25 @@ function Card({ title, description, info }) {
 }
 
 export default function () {
-    const [umsatz, setUmsatz] = useState(0);
+    const [revenue, setRevenue] = useState(0);
     const [profit, setProfit] = useState(0);
-    const [profitProvisionsrate, setProfitProvisionsrate] = useState(0);
-    const [trinkgeld, setTrinkgeld] = useState(0);
-    const [verkaufer, setVerkaufer] = useState(0);
-    const [trinkgeldProVerkauf, setTrinkgeldProVerkauf] = useState(0);
+    const [provision, setProvision] = useState(0);
+    const [tips, setTips] = useState(0);
+    const [totalSellerCount, setTotalSellerCount] = useState(0);
+    const [tipPerCustomer, setTipPerCustomer] = useState(0);
 
     useEffect(() => {
         fetch('http://localhost:8080/api/analytics', { method: 'GET' })
             .then(res => res.json())
             .then(data => {
                console.log(data)
-            }
-            )
+                setRevenue(data.Revenue)
+                setProvision(data.Provision)
+                setProvision(data.Provision)
+                setTips(data.Tips)
+                setTotalSellerCount(data.Sellers)
+                setTipPerCustomer(data.Tips / data.Offers)
+            })
     }, []);
 
     return (
@@ -33,15 +38,15 @@ export default function () {
                 <p className='mb-4'>Wow, was ein toller Basar! Hier siehst du noch einmal schwarz auf weiß, wie gut er gelaufen ist - vielleicht kannst du aus den Informationen fürs nächste Mal lernen.</p>
                 <div class="grid grid-cols-3 gap-6">
 
-                    <Card title={`${umsatz}€`} description="Umsatz"></Card>
-                    <Card title={`${profit}€`} description="Profit bei aktueller Provisionsrate (5%)"></Card>
-                    <Card title={<><span className='text-ourPrimaryColor'>{trinkgeld}€</span></>} description="Trinkgeld" info=" Das Trinkgeld musst du manuell eintragen"></Card>
+                    <Card title={`${revenue}€`} description="Umsatz"></Card>
+                    <Card title={`${revenue * provision/100}€`} description={`Profit bei aktueller Provisionsrate (${provision}%)`}></Card>
+                    <Card title={<><span className='text-ourPrimaryColor'>{tips}€</span></>} description="Trinkgeld" info=" Das Trinkgeld musst du manuell eintragen"></Card>
 
                     <div class="col-span-2 row-span-2 rounded-md border border-ourLightGray bg-white p-4 shadow-md">
                     </div>
-                    <Card title={`${trinkgeldProVerkauf}€`} description="Ø Trinkgeld/Verkauf">
+                    <Card title={`${tipPerCustomer}€`} description="Ø Trinkgeld/Verkauf">
                     </Card>
-                    <Card title={verkaufer} description="Verkäufer"></Card>
+                    <Card title={totalSellerCount} description="Verkäufer"></Card>
                 </div>
             </div >
         </>
