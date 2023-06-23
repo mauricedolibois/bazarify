29d12a712036e190fa99e5452898db323acecf10
Samuel Riester <riestersamuel@icloud.com>
2023-06-23 14:44:47 +0200

    Enhanced the UI of step4

--- frontend/components/ablauf/step4.js
+++ frontend/components/ablauf/step4.js
@@ -1,7 +1,8 @@
 import SellerInformation from "../SellerInformation"
 import SendMailsButton from "../buttons/SendMailsButton"
 import ButtonBigColor from "../buttons/ButtonBigColor"
-import { UilEnvelopeUpload } from '@iconscout/react-unicons'
+import ButtonSmallJustIcon from "../buttons/ButtonSmallJustIcon"
+import { UilEnvelopeUpload, UilSearch } from '@iconscout/react-unicons'
 import UnderlindedInput from "../underlinedInput"
 import { useState, useEffect } from 'react'
 
@@ -43,13 +44,24 @@ export default function () {
         searchedSeller.slice(0, 5)
         //set searched seller
         setSearchedSeller(tmpSearchedSeller)
-        console.log("searched seller: ", searchedSeller)
+
+
     }
+
+
 
     //get all products from seller
     const handleSellerClick = (seller) => {
         console.log("seller clicked: ", seller)
         setClickedSellerID(seller.seller_id)
+        // Setze den Namen auf den Wert des ersten gesuchten Verkäufers oder auf "John Doe", wenn kein Verkäufer gefunden wurde
+
+        let firstName = seller.seller_name
+        let lastName = seller.seller_firstname
+        let wholeName = firstName + " " + lastName
+        setName(wholeName);
+
+
     }
 
     useEffect(() => {
@@ -76,6 +88,18 @@ export default function () {
         setSellerPayback(tmpSellerPayback)
     }
 
+    // Neu importiert; Funktion zum Bestimmen des Texts für den dritten Div-Inhalt basierend auf der Anzahl der nicht verkauften Produkte
+    const getUnsoldProductsText = () => {
+        if (unsoldProductsCount === 0) {
+            return 'Keine nicht verkaufte Produkte vorhanden';
+        } else {
+            return `${unsoldProductsCount} nicht verkaufte Produkte vorhanden`;
+        }
+    };
+
+    // Neu importiert
+    const [name, setName] = useState('Kein Verkäufer ausgewählt');
+    const unsoldProductsCount = 5;
 
     return (
         <>
@@ -83,39 +107,69 @@ export default function () {
                 <h1>4. Abholung</h1>
                 <p className='mb-4'>Schön, dass du so viel verkaufen konntest. Du solltest jetzt die Verkäufer benachrichtigen, dass sie ihren Erlös und ggf. ihre liegengebliebene Artikel abholen kommen können.</p>
                 <SendMailsButton></SendMailsButton>
-
                 <h2 className="mt-8">Infos zum Verkäufer finden</h2>
-                <UnderlindedInput id="sellerSearchBar" placeholder="Verkäufer suchen..." onChange={searchSeller}></UnderlindedInput>
-                <div>
-                    {searchedSeller.map((seller) => (
-                        <div key={seller.id} onClick={() => handleSellerClick(seller)}>
-                            <p>{seller.seller_name} {seller.seller_firstname}</p>
+
+                {// Suche
+                }
+                <div className='flex gap-4'>
+                    <div class="">
+                        <div class="relative mt-2 rounded-md shadow-sm">
+                            <div class="pointer-events-none absolute inset-y-0 left-2 flex items-center"></div>
+                            <input type="text" onChange={searchSeller} class="rounded py-2 px-4 text-ourSuperDarkGray placeholder:text-ourGray focus:outline-ourPrimaryColor" id="sellerSearchBar" placeholder="Verkäufer suchen..." />
+                            <div>
+                                {searchedSeller.map((seller) => (
+                                    <div key={seller.id} onClick={() => handleSellerClick(seller)} className="px-4 py-2 cursor-pointer bg-white border border-ourLightGray rounded hover:text-ourPrimaryColorHover">
+                                        <p className="text-sm">{seller.seller_name} {seller.seller_firstname}</p>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
-                    ))}
+                    </div>
+                    {//<ButtonSmallJustIcon tooltip="Verkäufer finden" icon={<UilSearch></UilSearch>}></ButtonSmallJustIcon>
+                    }
                 </div>
-                <div class="relative overflow-x-auto">
-                    <table class="w-full text-left text-sm text-ourGray dark:text-ourGray">
-                        <thead class="bg-ourLightGray text-xs uppercase text-ourDarkGray dark:bg-gray-700 dark:text-ourGray">
-                            <tr>
-                                <th scope="col" class="px-6 py-3">Product name</th>
-                                <th scope="col" class="px-6 py-3">Category</th>
-                                <th scope="col" class="px-6 py-3">Price</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {allProductsFromSeller.map((product) => (
-                                <tr class="border-b bg-white dark:border-ourDarkGray dark:bg-ourSuperDarkGray">
-                                    <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-ourSuperDarkGray dark:text-white">{product.product_name}</th>
-                                    <td class="px-6 py-4">{product.product_category}</td>
-                                    <td class="px-6 py-4">{product.product_price}</td>
+
+                {// Verkäufer-Info
+                }
+                {allProductsFromSeller.length !== 0 &&
+                    <div class="grid grid-cols-3 mt-4 bg-white rounded border-ourLightGray border ">
+                        <div class="flex justify-center items-center py-4">
+                            <p>{name}</p>
+                        </div>
+                        <div class="flex justify-center flex-col items-center border-l border-ourLightGray border-r py-4">
+                            <p className='font-semibold'>{sellerPayback}€</p>
+                            <p className='mt-4'>Erlös</p>
+                        </div>
+                        <div class="flex justify-between text-center items-center py-4 px-8">
+                            <p>{getUnsoldProductsText()}</p>
+                        </div>
+                    </div>
+                }
+
+                {// Tabelle
+                }
+                {allProductsFromSeller.length !== 0 &&
+                    <div class="relative mt-4 overflow-x-auto">
+                        <table class="w-full text-left text-sm text-ourGray dark:text-ourGray">
+                            <thead class="bg-ourLightGray text-xs uppercase text-ourDarkGray dark:bg-gray-700 dark:text-ourGray">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">Product name</th>
+                                    <th scope="col" class="px-6 py-3">Category</th>
+                                    <th scope="col" class="px-6 py-3">Price</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-                <div>
-                    <p>Geld zurück: {sellerPayback}</p>
-                </div>
+                            </thead>
+                            <tbody>
+                                {allProductsFromSeller.map((product) => (
+                                    <tr class="border-b bg-white dark:border-ourDarkGray dark:bg-ourSuperDarkGray">
+                                        <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-ourSuperDarkGray dark:text-white">{product.product_name}</th>
+                                        <td class="px-6 py-4">{product.product_category}</td>
+                                        <td class="px-6 py-4">{product.product_price}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                }
             </div>
         </>
     )
