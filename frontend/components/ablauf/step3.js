d05ead717a9db7bc41d0e61734bba84a628eaab5
jb266 <jb266@hdm-stuttgart.de>
2023-06-27 00:50:14 +0200

    quick and dirty fix remove Items

--- frontend/components/ablauf/step3.js
+++ frontend/components/ablauf/step3.js
@@ -157,8 +157,46 @@ export default function () {
                 Scanne dafür einfach die Codes der Produkte ein, welche ein Kunde kaufen möchte. Wenn du alle Verkäufe
                 eingescannt hast, kannst du weiter zum nächsten Schritt.
             </p>
+            {//TODO: Tabelle als Komponente auslagern
+            }
             {scannedProducts.length > 0 && (
-                <ProductTable data={scannedProducts}/>
+                    <div className="rounded border border-ourLightGrey bg-white mb-4">
+                      <div className="overflow-hidden">
+                        <table className="min-w-full text-left text-sm font-light rounded">
+                          <thead className="font-medium">
+                            <tr>
+                              <th scope="col" className="px-8 py-4">
+                                #
+                              </th>
+                              <th scope="col" className="px-8 py-4">
+                                Artikel
+                              </th>
+                              <th scope="col" className="px-8 py-4">
+                                Kategorie
+                              </th>
+                              <th scope="col" className="px-8 py-4">
+                                Preis
+                              </th>
+                              <th scope="col" className="px-8 py-4">
+                                Entfernen
+                              </th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {scannedProducts.map((product, index) => (
+                              <Step3TableRow
+                                key={index}
+                                counter={index + 1}
+                                name={product.product_name}
+                                category={product.product_category}
+                                price={product.product_price}
+                                removeItem={() => handleRemoveProduct(index)}
+                              />
+                            ))}
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
             )}
             <div>
                 <div className="flex flex-row justify-between px-8 py-4 mb-8 gap-32 border-ourLightGray border bg-white rounded ">
