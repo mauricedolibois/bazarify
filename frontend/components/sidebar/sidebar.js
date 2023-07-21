4b7a8b58f4fb69743402fb593d33514228effdf4
Samuel Riester <riestersamuel@icloud.com>
2023-07-21 20:03:57 +0200

    sidebar cleaner

--- frontend/components/sidebar/sidebar.js
+++ frontend/components/sidebar/sidebar.js
@@ -1,17 +1,13 @@
-import { UilAngleRight } from "@iconscout/react-unicons";
-import { UilApps } from "@iconscout/react-unicons";
-import { UilPlus } from "@iconscout/react-unicons";
-import { UilQuestionCircle } from "@iconscout/react-unicons";
+import { UilAngleRight, UilApps, UilPlus, UilQuestionCircle } from "@iconscout/react-unicons";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
-import { useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { BazarContext } from "../../pages/index.js";
 import SidebarButton from "../buttons/SidebarButton.js";
 import HelpPopup from "../popups/HelpPopup/HelpPopup.js";
 
-function Step({ step, text, currentStep, onClick }) {
+function Step({ step, text, currentStep }) {
   let stepClass = "text-sm text-ourGray";
-  if (step == currentStep) {
+  if (step === currentStep) {
     stepClass = "text-sm text-ourPrimaryColor font-bold";
   } else if (step < currentStep) {
     stepClass = "text-sm line-through";
@@ -27,19 +23,22 @@ function Step({ step, text, currentStep, onClick }) {
 }
 
 export default function Sidebar() {
-  let { step, setStep, currentBazar } = useContext(BazarContext);
+  const { step, setStep, currentBazar } = useContext(BazarContext);
+  const [bazar, setBazar] = useState("");
+
+  useEffect(() => {
+    setBazar(currentBazar);
+  }, [currentBazar]);
 
   function showDashboard() {
     setStep(0);
   }
 
   function goToNextStep() {
-    // If step < 5 then go to next step and show the text "Nächster Schritt"
     if (step < 5) {
       setStep(step + 1);
     } else {
       showDashboard();
-      // If step == 5 then go to step 1 and show the text "Basar abschließen"
     }
   }
 
@@ -47,44 +46,18 @@ export default function Sidebar() {
     setStep(step);
   }
 
-  // bazarname anzeigen
-  const [bazar, setBazar] = useState("");
-
-  useEffect(() => {
-    setBazar(currentBazar);
-  }, [currentBazar]);
-
   return (
     <>
-      <div class="flex h-screen flex-col bg-white min-w-[20%] max-w-md border-r border-ourLightGray">
-        <div class="flex items-center justify-between pt-4">
-          <div class="flex items-center cursor-pointer">
+      <div className="flex h-screen flex-col bg-white min-w-[20%] max-w-md border-r border-ourLightGray">
+        <div className="flex items-center justify-between pt-4">
+          <div className="flex items-center cursor-pointer">
             <Link onClick={showDashboard} href="">
-              <span class="pl-4 font-serif text-2xl text-ourPrimaryColor">
-                B
-              </span>
-              <span class="font-serif text-2xl">azarify</span>
+              <span className="pl-4 font-serif text-2xl text-ourPrimaryColor">B</span>
+              <span className="font-serif text-2xl">azarify</span>
             </Link>
           </div>
           <HelpPopup />
         </div>
-
-        {/*
-                <div class="flex flex-row items-center justify-center px-4 py-4">
-             
-                    <Link href="/template" className='flex-1'>
-                        <button class="border-ourGrey text-ourGrey rounded-lg border-2 flex flex-col justify-center px-4 py-2 flex-1">
-                            <UilPlus class="" size="24" />
-                            <span class="text-ourGrey text-sm mt-4">Neuer Bazar</span>
-                        </button>
-                    </Link>
-                    <button class="border-ourGrey text-ourGrey ml-2 flex flex-col justify-center rounded-lg border-2 px-4 py-2 flex-1">
-                        <UilApps class="" size="24" />
-                        <span class="text-ourGrey text-sm mt-4">Bazare verwalten</span>
-                    </button>
-                    
-                </div >
-                */}
 
         <div className="flex flex-row px-4 py-4 gap-4">
           <Link href="/" onClick={showDashboard} className="w-full">
@@ -92,34 +65,19 @@ export default function Sidebar() {
           </Link>
         </div>
 
-        <div class="border-t border-ourLightGray pb-4"></div>
+        <div className="border-t border-ourLightGray pb-4"></div>
 
-        <h3 class="px-4 py-2 text-lg font-bold text-ourSuperDarkGray">
+        <h3 className="px-4 py-2 text-lg font-bold text-ourSuperDarkGray">
           {step === 0 ? "Ablauf" : currentBazar.replaceAll("_", " ")}
         </h3>
 
-        {/*}
-                <h3 class="px-4 py-2 text-lg font-bold text-ourSuperDarkGray">Ablauf</h3>
-                {*/}
-
-        <ul class="flex flex-grow flex-col space-y-2 px-4 py-2">
-          <Step step="1" text="Basar erstellen" currentStep={step} />
-          <Step step="2" text="Annahme" currentStep={step} />
-          <Step step="3" text="Verkauf" currentStep={step} />
-          <Step step="4" text="Abholung" currentStep={step} />
-          <Step step="5" text="Analytics und Bilanz" currentStep={step} />
+        <ul className="flex flex-grow flex-col space-y-2 px-4 py-2">
+          <Step step={1} text="Basar erstellen" currentStep={step} />
+          <Step step={2} text="Annahme" currentStep={step} />
+          <Step step={3} text="Verkauf" currentStep={step} />
+          <Step step={4} text="Abholung" currentStep={step} />
+          <Step step={5} text="Analytics und Bilanz" currentStep={step} />
         </ul>
-
-        {/*
-                <div class="mt-auto border-t border-ourLightGray">
-                    <button onClick={goToNextStep} class="justify-left flex h-10 w-full items-center pl-4 text-sm text-ourPrimaryColor hover:bg-ourPrimaryColorHover hover:text-white">
-                        <UilAngleRight size="24" />
-                        {step === 0 && "Los geht's!"}
-                        {(step < 5 && step > 0) && "Nächster Schritt"}
-                        {step == 5 && "Basar abschließen"}
-                    </button>
-                </div>
-                */}
       </div>
     </>
   );
