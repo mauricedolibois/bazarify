657a06d2bd22b7ddf4ada44dc8410e290d8bd69d
Samuel Riester <riestersamuel@icloud.com>
2023-07-21 20:05:37 +0200

    dashboard cleaner

--- frontend/components/pages/dashboard.js
+++ frontend/components/pages/dashboard.js
@@ -1,13 +1,24 @@
 import { UilTrashAlt, UilNoEntry, UilPlus } from "@iconscout/react-unicons";
-import ButtonBigColor from "../buttons/ButtonBigColor";
-import ButtonBigNoColor from "../buttons/ButtonBigNoColor";
 import Link from "next/link";
 import { useContext, useState, useEffect } from "react";
 import { BazarContext } from "../../pages/index.js";
 import VideoPopup from "../popups/VideoPopup/VideoPopup";
 import BazarCard from "../BazarCard/BazarCard";
+import ButtonBigColor from "../buttons/ButtonBigColor";
+import ButtonBigNoColor from "../buttons/ButtonBigNoColor";
 
-export default function dashboard() {
+export default function Dashboard() {
+  const { setStep } = useContext(BazarContext);
+  const [bazars, setBazars] = useState(undefined);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/getBazars")
+      .then((res) => res.json())
+      .then((data) => {
+        setBazars(data);
+      });
+  }, []);
+
   function loadExampleData() {
     fetch("http://localhost:8080/api/loadExampleData", {
       method: "PUT",
@@ -22,17 +33,6 @@ export default function dashboard() {
       .catch((error) => console.log(error));
   }
 
-  let { setStep } = useContext(BazarContext);
-  const [bazars, setBazars] = useState(undefined);
-
-  useEffect(() => {
-    fetch("http://localhost:8080/api/getBazars")
-      .then((res) => res.json())
-      .then((data) => {
-        setBazars(data);
-      });
-  }, []);
-
   return (
     <>
       <div>
@@ -44,34 +44,34 @@ export default function dashboard() {
           führen dich Schritt für Schritt durch den gesamten Prozess, damit
           dieser dein einfachster Basar bisher wird!
         </p>
-        <div class="flex flex-row gap-4">
+        <div className="flex flex-row gap-4">
           {/* Sollen wir das hier lieber mit dem Stepper machen (so wie in der Sidebar, also onClick={showDashboard}) oder mit href? */}
           <Link href="/" onClick={() => setStep(1)}>
             <ButtonBigColor
               text="Neuen Basar erstellen"
               icon={<UilPlus />}
-            ></ButtonBigColor>
+            />
           </Link>
           <Link href="/">
-            <VideoPopup></VideoPopup>
+            <VideoPopup />
           </Link>
-          <Link href="/" onClick={() => loadExampleData()}>
+          <Link href="/" onClick={loadExampleData}>
             <ButtonBigNoColor
               text="Demoversion laden"
               icon={<UilNoEntry />}
-            ></ButtonBigNoColor>
+            />
           </Link>
         </div>
         <h2 className="mt-16">Deine Basare</h2>
-        <div class="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-3 gap-4">
           {bazars === undefined ? (
             <p>Keine Basare vorhanden</p>
           ) : (
-            bazars.map((bazar) => {
-              return bazar.hasOwnProperty("bazar_name") ? (
-                <BazarCard name={bazar.bazar_name} />
-              ) : null;
-            })
+            bazars.map((bazar) =>
+              bazar.hasOwnProperty("bazar_name") ? (
+                <BazarCard name={bazar.bazar_name} key={bazar.id} />
+              ) : null
+            )
           )}
         </div>
       </div>
