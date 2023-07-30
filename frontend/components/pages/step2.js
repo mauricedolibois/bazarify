2dd98e8b2ef14e19fee4298de9e36f3b9593ed7a
jb266 <jb266@hdm-stuttgart.de>
2023-07-30 15:38:54 +0200

    remove print comments

--- frontend/components/pages/step2.js
+++ frontend/components/pages/step2.js
@@ -94,33 +94,6 @@ export default function () {
     }
   }, [validProductName, validProductCategory, validProductPrice]);
 
-  //TODO: beim router im backend array abgreifen und dann printen
-  // useEffect(() => {
-  //   if (product !== '') {
-  //     fetch('http://localhost:8080/api/addPendingProduct', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify({
-  //         product: product,
-  //       }),
-  //     })
-  //       .then((res) => {
-  //         if (!res.ok) {
-  //           throw new Error('Failed to add pending product');
-  //         }
-  //         return res.json();
-  //       })
-  //       .then(() => {
-  //         setPendingProducts((pendingProducts) => [...pendingProducts, { product: product }]);
-  //       })
-  //       .catch((error) => {
-  //         console.log(error);
-  //       });
-  //   }
-  // }, [product]);
-
   const handleAddOffer = async () => {
     if (pendingProducts.length !== 0) {
       console.log("pendingProducts .... :", pendingOffers);
@@ -213,49 +186,6 @@ export default function () {
       setSellerSubmitted(false);
     }
   }, [seller]);
-
-  // async function sendPendingOffers(){
-  //   if (pendingProducts.length > 0) {
-  //     try {
-  //       const offerPromises = pendingProducts.map(async (pendingProduct) => {
-  //         console.log(pendingProduct.product)
-  //         console.log(seller)
-  //         const response = await fetch('http://localhost:8080/api/offer', {
-  //           method: 'POST',
-  //           headers: {
-  //             'Content-Type': 'application/json',
-  //           },
-  //           body: JSON.stringify({
-  //             product: pendingProduct.product,
-  //             seller: seller,
-  //           }),
-  //         });
-
-  //         const data = await response.json();
-
-  //         return data;
-  //       });
-
-  //       //const offers = await Promise.all(offerPromises);
-
-  //       // const printResponse = await fetch('http://localhost:8080/api/PrintAllOffers', {
-  //       //   method: 'PUT',
-  //       //   headers: {
-  //       //     'Content-Type': 'application/json',
-  //       //   },
-  //       //   body: JSON.stringify({
-  //       //     offers: offers,
-  //       //   }),
-  //       // });
-
-  //       // Handle the response if needed
-  //     } catch (error) {
-  //        console.log(error);
-  //    }
-
-  //    setPendingProducts([]);
-  //   }
-  // }
 
   return (
     <>
